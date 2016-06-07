dd0023dbba47d35b7728f5b0b5a05b11ffa8c18f
Iva <Iva@Ivas-MacBook-Pro.local>
2016-06-07 19:26:22 +0200

    Swappy server side v1

---
+++ server/boot/script.js
@@ -0,0 +1,45 @@
+/*globals debug */
+module.exports = function (app) {
+  var cloudantDB = app.dataSources.cloudant;
+  cloudantDB.automigrate('accounts', function (err) {
+    if (err) throw (err);
+    var accounts = app.models.accounts;
+    accounts.find({ where: { username: 'Admin' }, limit: 1 }, function (err, users) {
+
+      if (!users) {
+
+
+        accounts.create([
+          { username: 'Admin', email: 'admin@admin.com', password: 'abcdef' }
+        ], function (err, users) {
+          if (err) return debug(err);
+
+          var Role = app.models.Role;
+          var RoleMapping = app.models.RoleMapping;
+
+          Role.destroyAll();
+          RoleMapping.destroyAll();
+
+          //create the admin role
+          Role.create({
+            name: 'admin'
+          }, function (err, role) {
+            if (err) return debug(err);
+
+            //make admin
+            role.principals.create({
+              principalType: RoleMapping.USER,
+              principalId: users[0].id
+            }, function (err, principal) {
+              if (err) throw (err);
+            });
+          });
+        })
+      }
+      else {
+
+      }
+
+    });
+  });
+};
